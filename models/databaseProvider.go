e5a29cbabaa16b42f60f851c823597d43deda16c
sj <sebastianthegreatful@gmail.com>
2021-03-30 19:58:19 +0700

    Implement providers for both Sqlite and Postgres

--- models/databaseProvider.go
+++ models/databaseProvider.go
@@ -1,27 +1,53 @@
 package models
 
 import (
+	"fmt"
+	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+	"os"
 )
 
-// TODO: take Config as argument
 func ProvideDatabase() *gorm.DB {
-	/*
-		host, _ := os.LookupEnv("DATABASE_HOST")
-		port, _ := os.LookupEnv("DATABASE_PORT")
-		username, _ := os.LookupEnv("DATABASE_USERNAME")
-		password, _ := os.LookupEnv("DATABASE_PASSWORD")
-		name, _ := os.LookupEnv("DATABASE_NAME")
+	database := provideSqliteDatabase()
+	//	database := providePostgresDatabase()
 
-		dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, username, password, name, port)
-	*/
+	_ = database.AutoMigrate(
+		&Race{},
+		&Heat{},
+		&Runner{},
+	)
+
+	return database
+}
+
+func providePostgresDatabase() *gorm.DB {
+	host, _ := os.LookupEnv("DATABASE_HOST")
+	port, _ := os.LookupEnv("DATABASE_PORT")
+	username, _ := os.LookupEnv("DATABASE_USERNAME")
+	password, _ := os.LookupEnv("DATABASE_PASSWORD")
+	name, _ := os.LookupEnv("DATABASE_NAME")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, username, password, name, port)
+
 	config := gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	}
 
-	//	database, err := gorm.Open(postgres.Open(dsn), &config)
+	database, err := gorm.Open(postgres.Open(dsn), &config)
+
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+
+	return database
+}
+
+func provideSqliteDatabase() *gorm.DB {
+	config := gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	}
 
 	database, err := gorm.Open(sqlite.Open("test.db"), &config)
 
